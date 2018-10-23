1cedad49dc9b6aaa526b83a93786406d3823a002
JuanCruz <jjuancruzrey@gmail.com>
2018-10-23 07:26:08 -0300

    Agregando Categorias

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Categorias from './src/playlist/components/categorias.js';
+import Playlist from './src/playlist/components/playlist';
 import data from './src/api.json';
 // console.log('Hola mundo!')
 
 const app = document.getElementById('app')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>hola Estudiante!</h1>;
+render( <Playlist data={data} />, app);
 
-
-// render( <Playlist data={data} />, app);
-render(<Categorias data={data}/>, app);
